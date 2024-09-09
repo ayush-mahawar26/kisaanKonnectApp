797e9236345201359314ca3b5331547aae3de274
AyushMahawar <ayushmahawar60@gmail.com>
2024-09-10 05:29:26 +0530

    spaceerror

--- lib/constants/global.constants.dart
+++ lib/constants/global.constants.dart
@@ -1,7 +1,7 @@
 import 'package:kisaan_konnect/models/marketplace.item.model.dart';
 
 class GlobalConstants {
-  static String url = "https://kisaan-konnect.onrender.com ";
+  static String url = "https://kisaan-konnect.onrender.com";
   static Map<String, String> header = {
     "Content-type": "application/json",
     "Accept": "application/json",
